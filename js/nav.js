0633e3f7ed96e732dcdfa6ce4d0a055d7ab6d29d
Magfur <33932322+mjafory@users.noreply.github.com>
2024-02-04 23:54:04 -0500

    updated

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,11 @@
 function openNav() {
-  document.getElementById("mySidenav").style.width = "100%";
+	document.getElementById("mySidenav").style.width = "100%";
 }
 
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+	document.getElementById("mySidenav").style.width = "0";
 }
+
+
+const currentYear = new Date().getFullYear();
+document.getElementById("current_year").innerHTML = currentYear;
